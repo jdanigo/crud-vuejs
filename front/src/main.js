a5ecaa43b9f1d508628343106786c7e707c203b3
User <user@MacBook-Pro-de-User.local>
2021-12-08 23:09:06 -0500

    front

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router'
 
-createApp(App).mount('#app')
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
